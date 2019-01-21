0123316fad92e5c619ef590cf8ee8770fd4e5c18
MikaelaJ <mikaela_johansson@hotmail.com>
2019-01-21 15:30:25 +0100

    test2

--- script.php
+++ script.php
@@ -2,3 +2,11 @@ function addNumbers($a, $b) {
   $sum = $a + $b;
   return $sum;
 }
+
+function addNumbers($numbers) {
+  $sum = $a + $b;
+  foreach($numbers as $number) {
+    $sum += $number;
+  }
+  return $sum;
+}
